48567f84523165d6f5ae5e1805d1c50bb58171c0
Arahk Yambupah <arahk@outlook.com>
2025-07-30 11:05:06 +0700

    Block add duplicate title Task

--- src/application/Task/CreateTask/CreateTaskValidator.cs
+++ src/application/Task/CreateTask/CreateTaskValidator.cs
@@ -1,12 +1,16 @@
 using System.ComponentModel.DataAnnotations;
 using Arahk.MyMediatr;
+using Arahk.TaskNova.Lib.Domain;
 
 namespace Arahk.TaskNova.Lib.Application.Task.CreateTask;
 
-public class CreateTaskValidator : IRequestValidator<CreateTaskRequest>
+public class CreateTaskValidator(ITaskRepository taskRepository) : IRequestValidator<CreateTaskRequest>
 {
     public async Task<bool> ValidateAsync(CreateTaskRequest request, CancellationToken cancellationToken)
     {
-        return await System.Threading.Tasks.Task.FromResult(request.Title != "Test");
+        var existedTask = await taskRepository.GetAllTasksAsync();
+        var isDuplicate = existedTask.Any(p => p.Title == request.Title);
+
+        return await System.Threading.Tasks.Task.FromResult(!isDuplicate);
     }
 }
